3cf5130f7a276a719eb9a8cb25612ce96ee996cc
Miguel Totti <migueltotti2005@gmail.com>
2026-01-19 20:53:04 -0300

    feat: add price propertie to Product and ajust excel file generation

--- GeradorListaAssados.Console/Product.cs
+++ GeradorListaAssados.Console/Product.cs
@@ -5,18 +5,25 @@ namespace GeradorListaAssados.Console;
 public class Product
 {
     public string Name { get; private set; }
+    public decimal Price { get; private set; }
     public int Quantity { get; private set; }
     public int Index { get; private set; }
     public string HexCodeColor { get; private set; }
 
-    public Product(string name, int quantity, int index, string hexCodeColor)
+    public Product(string name, decimal price, int quantity, int index, string hexCodeColor)
     {
         Name = name;
+        Price = price;
         Quantity = quantity;
         Index = index;
         HexCodeColor = hexCodeColor;
     }
 
+    public void SetPrice(decimal price)
+    {
+        Price = price;
+    }
+    
     public void SetQuantity(int quantity)
     {
         Quantity = quantity;
--- GeradorListaAssados.Console/Program.cs
+++ GeradorListaAssados.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using ClosedXML.Excel;
+﻿using System.Globalization;
+using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Spreadsheet;
 using GeradorListaAssados.Console;
 using Color = System.Drawing.Color;
@@ -38,25 +39,25 @@ worksheet.Cell("B2").Style.Alignment.Vertical = XLAlignmentVerticalValues.Center
 // Preparar sample de assados
 var assadosList = new List<Product>
 {
-    new Product("FRANGO", 21, 1, "#FFD966"),
-    new Product("FRANGO DESOSSADO RECHEADO TOMATE", 1, 2, "#FFD966"),
-    new Product("FRANGO DESOSSADO RECHEADO CALABRESA", 1, 3, "#FFD966"),
-    new Product("FRANGO DESOSSADO RECHEADO BATATA", 1, 4, "#FFD966"),
-    new Product("FRANGO DESOSSADO RECHEADO FAROFA", 3, 5, "#FFD966"),
+    new Product("FRANGO", 46.90m, 21, 1, "#FFD966"),
+    new Product("FRANGO DESOSSADO RECHEADO TOMATE", 74.90m, 1, 2, "#FFD966"),
+    new Product("FRANGO DESOSSADO RECHEADO CALABRESA", 74.90m, 1, 3, "#FFD966"),
+    new Product("FRANGO DESOSSADO RECHEADO BATATA", 74.90m, 1, 4, "#FFD966"),
+    new Product("FRANGO DESOSSADO RECHEADO FAROFA", 74.90m, 3, 5, "#FFD966"),
     
-    new Product("COXA COM SOBRECOXA DESOSS. RECH   33 UND", 8, 6, "#FFD966"),
-    new Product("COPA LOMBO", 2, 7, "#9BC2E6"),
-    new Product("COSTELA SUINA", 4, 8, "#9BC2E6"),
-    new Product("JOELHO", 2, 9, "#9BC2E6"),
-    new Product("PANCETA", 6, 10, "#9BC2E6"),
-    new Product("COSTELINHA CROCK", 2, 11, "#9BC2E6"),
+    new Product("COXA COM SOBRECOXA DESOSS. RECH   33 UND", 0, 8, 6, "#FFD966"),
+    new Product("COPA LOMBO", 0, 2, 7, "#9BC2E6"),
+    new Product("COSTELA SUINA", 0, 4, 8, "#9BC2E6"),
+    new Product("JOELHO", 0, 2, 9, "#9BC2E6"),
+    new Product("PANCETA", 0, 6, 10, "#9BC2E6"),
+    new Product("COSTELINHA CROCK", 0, 2, 11, "#9BC2E6"),
     
-    new Product("FRALDINHA", 5, 12, "#EF6FC1"),
-    new Product("MAMINHA", 3, 13, "#EF6FC1"),
-    new Product("MAMINHA RECHEADA", 3, 14, "#EF6FC1"),
-    new Product("COSTELA BOVINA", 6, 15, "#EF6FC1"),
-    new Product("CUPIM", 4, 16, "#EF6FC1"),
-    new Product("PONTA DE PEITO", 1, 17, "#EF6FC1"),
+    new Product("FRALDINHA", 0, 5, 12, "#EF6FC1"),
+    new Product("MAMINHA", 0, 3, 13, "#EF6FC1"),
+    new Product("MAMINHA RECHEADA", 0, 3, 14, "#EF6FC1"),
+    new Product("COSTELA BOVINA", 0, 6, 15, "#EF6FC1"),
+    new Product("CUPIM", 0, 4, 16, "#EF6FC1"),
+    new Product("PONTA DE PEITO", 0, 1, 17, "#EF6FC1"),
 };
 
 assadosList.Sort((p1, p2) => p1.Index < p2.Index ? -1 : 1);
@@ -75,7 +76,8 @@ foreach (var assado in assadosList)
     worksheet.Row(row).Height = 35.1;
     
     // Prepara titulo do assado
-    worksheet.Cell(row, column).Value = assado.Name;
+    var formatedPrice = assado.Price.ToString("N2", new CultureInfo("pt-BR"));
+    worksheet.Cell(row, column).Value = $"{assado.Name} R$ {formatedPrice}";
     worksheet.Cell(row, column).Style.Font.FontSize = 14;
     worksheet.Cell(row, column).Style.Font.Bold = true;
     worksheet.Cell(row, column).Style.Font.FontName = WorksheetConstants.FontName;
@@ -90,7 +92,7 @@ foreach (var assado in assadosList)
     var initialRow = row;
     
     // Prepara quantidade de caixas correspondente à quantidade desse assado
-    for (int i = 1; i <= assado.Quantity; i++)
+    for (var i = 1; i <= assado.Quantity; i++)
     {
         worksheet.Row(row).Height = 35.1;
         
